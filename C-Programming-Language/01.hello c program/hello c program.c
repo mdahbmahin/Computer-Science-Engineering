9127cdf385fa5c19f721e178c589bb1fdbee9d05
Md. Ahb Mahin <89274582+mdahbmahin@users.noreply.github.com>
2023-03-28 22:33:12 +0600

    Add files via upload

---
+++ C-Programming-Language/01.hello c program/hello c program.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main(){
+
+printf("Hello C Program");
+printf("Hello Bangladesh");
+printf("Hello Windows 10 pro - 7CZN7-BGNZ5-MW6VV-TVYTM-3AWS6 ");  
+printf("Hello Windows 8 - SAHKR-2ZTRR-BVVA3-EYJVB-XKRBH ");
+printf("Hello Windows 7 - KW5P4-Q44WG-9L66V-GQM84-L4MFH ");
+
+
+return 0;
+}
