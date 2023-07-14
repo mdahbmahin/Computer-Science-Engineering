046132799a3e29509c83cc6284351ac62b3fbdbb
MD AHB MAHIN <89274582+mdahbmahin@users.noreply.github.com>
2023-07-15 00:52:03 +0600

    Update Print-sigma-rules.c

--- C-Programming-Language/Print-sigma-rules.c
+++ C-Programming-Language/Print-sigma-rules.c
@@ -1,9 +1,25 @@
 #include<stdio.h>
 int main(){
   
-printf("beautiful sigma male rules");
-printf("primary sigma rule of men #1 - :   They have an ambiguous morality");
-printf("secondary sigma rule of men #2 - : Take Full Responsibility");
+printf("best beautiful sigma male rules");
+  
+printf("sigma rule of gitmen #0158 - :   They have an ambiguous morality");
+printf("sigma rule of gitmen #2016 - :   Take Full Responsibility");
 
-return 0;
+printf("sigma rule of introvertmen #0158 - :   Be calm");
+printf("sigma rule of introvertmen #2016 - :   you are my bird "); // hahahahahahahha
+
+printf("sigma rule number #0158  for gama men  - :");
+printf("sigma rule number #1106  for gama men  - :");
+printf("sigma rule number #2016  for gama men  - :");
+printf("sigma rule number #m1106 for gama men  - :");
+
+printf("sigma rule number #0021  for faminist  - :"); //hahahahaha
+printf("sigma rule number #0021  for faminist  - :"); //hahahahaha
+printf("sigma rule number #0021  for faminist  - :"); //hahahahaha 
+
+printf("sigma rule number #0021  for motherfucker  - :"); //hahahahaha
+printf("sigma rule number #0078  for motherfucker  - :"); //hahahahaha
+
+  return 0;
 }
