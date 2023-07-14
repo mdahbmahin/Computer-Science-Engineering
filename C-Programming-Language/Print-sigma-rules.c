ea47755722b482e60a4bdcdaf0bf28cbc539d431
MD AHB MAHIN <89274582+mdahbmahin@users.noreply.github.com>
2023-07-14 15:21:28 +0600

    Update Print-sigma-rules.c

--- C-Programming-Language/Print-sigma-rules.c
+++ C-Programming-Language/Print-sigma-rules.c
@@ -3,7 +3,7 @@ int main(){
   
 printf("beautiful sigma male rules");
 printf("primary sigma rule of men #1 - :   They have an ambiguous morality");
-
+printf("secondary sigma rule of men #2 - : Take Full Responsibility");
 
 return 0;
 }
